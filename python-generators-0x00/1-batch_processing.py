37a340ad8a26f454f6a152ed52f38f07fdc014b1
Yosef <yosefetene02@gmail.com>
2025-05-18 01:42:26 +0300

    Updated batch processing scripts

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -10,51 +10,35 @@ def stream_users_in_batches(batch_size):
     session = Session()
 
     with Session() as session:
-        counter = 0
+        batch = []
         # "FROM user_data", "SELECT"
         stmt = select(User)
         user_obj = session.scalars(stmt)
         for user in user_obj:
-            counter += 1
-            if counter > batch_size:
-                break
-            yield {
-                'user_id': user.user_id,
-                'name': user.name,
-                'email': user.email,
-                'age': user.age
-            }            
+            batch.append(user)
+            if len(batch) == batch_size:
+                yield batch
+                batch = []
 
+        
+        if batch:
+            yield batch
 
 def batch_processing(batch_size):
 
-    engine = create_engine('mysql+pymysql://alx_user:alx_password@192.168.96.2:3306/ALX_prodev')
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    with Session() as session:
-        counter = 0
-        stmt = select(User)
-        user_obj = session.scalars(stmt)
-        for user in user_obj:
-            counter += 1
-            if counter > batch_size:
-                break
-            if user.age <= 25:
-                if counter > 0:
-                    counter -= 1
-                continue
+   for batch in stream_users_in_batches(batch_size):
+    for user in batch:
+        if user.age > 25:
             yield {
                 'user_id': user.user_id,
                 'name': user.name,
                 'email': user.email,
                 'age': user.age
             }            
-
 
 
 
 if __name__ == '__main__':
-    
+    count = 0
     for user in batch_processing(2):
         print(user)
