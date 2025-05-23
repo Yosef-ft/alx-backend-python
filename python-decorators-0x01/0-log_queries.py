ee824f58a8f4b37049702eb8fffb0a27bfa0edae
Yosef <yosefetene02@gmail.com>
2025-05-24 00:09:04 +0300

    Logging database queries

---
+++ python-decorators-0x01/0-log_queries.py
@@ -0,0 +1,20 @@
+import sqlite3
+import functools
+
+#### decorator to log SQL queries
+def log_queries(func):
+    def print_queries(query):
+        print(query)
+    return print_queries
+
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+#### fetch users while logging the query
+users = fetch_all_users(query="SELECT * FROM users")
---
+++ python-decorators-0x01/seed.sql
@@ -0,0 +1,10 @@
+-- Active: 1747702231422@@192.168.128.2@3306
+CREATE TABLE users(
+  name varchar, 
+  age int,
+  phone_number varchar
+);
+
+
+insert into users(name, age, phone_number) VALUES
+('John', 21, '099999395');
