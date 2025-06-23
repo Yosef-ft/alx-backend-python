57b9fc62fb698b28c2931a0d0b44f56cce98076d
Yosef <yosefetene02@gmail.com>
2025-06-24 00:52:50 +0300

    Added models, migrations

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -2,4 +2,17 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class users(AbstractUser):
-  pass
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    birthdate = models.DateField(blank=True, null=True)	
+    
+
+class Conversation(models.Model):
+    participants = models.ManyToManyField(users)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+
+class Message(models.Model):
+	conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE)
+	sender = models.ForeignKey(users, on_delete=models.CASCADE)
+	content = models.TextField()
+	timestamp = models.DateTimeField(auto_now_add=True)
--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -128,3 +128,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = "chats.users"
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
