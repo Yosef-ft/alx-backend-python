9c986ec491c900393b56ce0abf2e7535e2d0bd9b
Yosef <yosefetene02@gmail.com>
2025-06-24 09:27:40 +0300

    Added views

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -1,17 +1,48 @@
 from rest_framework import serializers
 from .models import users, Conversation, Message
-
+from rest_framework.serializers import ValidationError
 
 class UsersSerializer(serializers.ModelSerializer):
+
+	full_name = serializers.SerializerMethodField()
+
 	class Meta:
 		model = users
+		fields = ['user_id', 'email', 'first_name', 'last_name', 'full_name', 'password']
+
+		extra_kwargs = {
+			'password' : {'write_only': True}
+		}
+
+	
+	def get_full_name(self, obj):
+		return f"{obj.first_name} {obj.last_name}"
+	
+	def validate_password(self, value):
+
+		if len(value) < 8:
+			raise ValidationError("Password must be at least 8 chars long")
+		return value
+	
+	def create(self, validated_data):
+		user = users.objects.create(
+            username=validated_data['email'],
+            email=validated_data['email'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', ''),
+            password=validated_data['password']
+		)
+		return user
 
 
 class ConversationSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Conversation
+		# serializers.CharField
+		fields = "__all__"
 
 
 class MessageSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Message
+		fields = "__all__"
--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
+from rest_framework.viewsets import ViewSet, ModelViewSet
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import action 
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer, UsersSerializer
 
-# Create your views here.
+
+class ConversationViewSet(ViewSet):
+	queryset = Conversation.objects.all()
+
+	def list(self, request):
+		serializer = ConversationSerializer(self.queryset, many=True)
+		return Response(serializer.data)
+	
+
+	def create(self, request, *args, **kwargs):
+		serializer = ConversationSerializer(data=request.data)
+		if serializer.is_valid(raise_exception=True):
+			serializer.save()
+			return Response(serializer.data, status=status.HTTP_201_CREATED)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class MessageViewSet(ViewSet):
+	queryset = Message.objects.all()
+
+
+	def list(self, request):
+		serializer =  MessageSerializer(self.queryset, many=True)
+		return Response(serializer.data)
+	
+
+	@action(detail=False, methods=['post'])
+	def send_message(self, request):
+		serializer = MessageSerializer(data=request.data)
+		if serializer.is_valid(raise_exception=True):
+			serializer.save(sender=request.user)
+			return Response(serializer.data, status=status.HTTP_201_CREATED)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
