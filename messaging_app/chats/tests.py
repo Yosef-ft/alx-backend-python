02c8755925e7cd7d999a95fc33ef37a26f64db87
Yosef <yosefetene02@gmail.com>
2025-07-06 02:39:05 +0300

    Added jenkinsfile and added test

--- messaging_app/chats/tests.py
+++ messaging_app/chats/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import users
 
-# Create your tests here.
+class UserTestCase(TestCase):
+    def test_create_user(self):
+        """Test that a user can be created"""
+        user = users.objects.create_user(
+            email='test@example.com',
+            username='testuser',
+            password='password123',
+            first_name='Test',
+            last_name='User'
+        )
+        self.assertEqual(user.email, 'test@example.com')
+        self.assertEqual(user.username, 'testuser')
+        self.assertEqual(user.first_name, 'Test')
+        self.assertEqual(user.last_name, 'User')
